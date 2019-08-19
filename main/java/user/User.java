0f877ae5c679ce96128eb0867420fedcd1f5043a
Ankitagole <ankita17@navgurukul.org>
2019-08-19 19:19:49 +0530

    test case for price and quantity

--- main/java/user/User.java
+++ main/java/user/User.java
@@ -5,6 +5,7 @@ import shoppingCart.ProductDetail;
 public class User {
     public static void main(String[] args) {
         ProductDetail productDetail = new ProductDetail(1,20, "food", "chips");
-        System.out.println(productDetail.category);
+        ProductDetail productDetail1 = new ProductDetail(1,10, "food", "chips");
+        System.out.println(productDetail1.quantity + productDetail.quantity);
     }
 }
--- test/java/shoppingCartTest/ShoppingTest.java
+++ test/java/shoppingCartTest/ShoppingTest.java
@@ -1,4 +1,21 @@
 package shoppingCartTest;
-public class ShoppingTest {
 
+import org.junit.Test;
+import shoppingCart.ProductDetail;
+
+import static org.junit.Assert.assertEquals;
+
+public class ShoppingTest {
+    @Test
+    public void itShouldGiveThePriceOfProduct(){
+        ProductDetail productDetail = new ProductDetail(1,20, "food", "chips");
+        double expected = 20;
+        assertEquals(expected,productDetail.price,1);
+    }
+    @Test
+    public void itShouldGiveTheQuantityOfProduct(){
+        ProductDetail productDetail = new ProductDetail(1,20, "food", "chips");
+        int expected = 1;
+        assertEquals(expected,productDetail.quantity);
+    }
 }
