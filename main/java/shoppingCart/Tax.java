4fb6fcf617e57b15aa28d263fc9ff0c8cd38eb48
Ankitagole <ankita17@navgurukul.org>
2019-08-20 18:48:32 +0530

    refactor. variable name

--- main/java/shoppingCart/Tax.java
+++ main/java/shoppingCart/Tax.java
@@ -1,15 +1,15 @@
 package shoppingCart;
 
 public class Tax {
-    final int salesTax = 10;
-    final double importTax = 5;
-    double tax = 0.0;
+    final int SALESTAX = 10;
+    final double IMPORTTAX = 5;
+    private double tax = 0.0;
     public double getTax(double amount, boolean importedProduct,boolean taxable) {
         if (taxable) {
             if (importedProduct) {
-                return (amount * importTax / 100);
+                return (amount * IMPORTTAX / 100);
             } else {
-                return (amount * salesTax / 100);
+                return (amount * SALESTAX / 100);
             }
         }
         return tax;
