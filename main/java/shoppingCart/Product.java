609921b2a9d6b7574e967a3e55caa9a7ba7a00be
Ankitagole <ankita17@navgurukul.org>
2019-08-21 12:30:43 +0530

    override the equals

--- main/java/shoppingCart/Product.java
+++ main/java/shoppingCart/Product.java
@@ -1,14 +1,38 @@
 package shoppingCart;
+
+import java.util.Objects;
+
 public class Product {
     public double price;
     public String name;
     boolean importedProduct;
     String category;
 
-    public Product(int price, String name, boolean importedProduct,String category) {
+    public Product(int price, String name, boolean importedProduct, String category) {
         this.price = price;
         this.name = name;
         this.importedProduct = importedProduct;
         this.category = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return Double.compare(product.price, price) == 0 &&
+                importedProduct == product.importedProduct &&
+                Objects.equals(name, product.name) &&
+                Objects.equals(category, product.category);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "price=" + price +
+                ", name='" + name + '\'' +
+                ", importedProduct=" + importedProduct +
+                ", category='" + category + '\'' +
+                '}';
     }
 }
