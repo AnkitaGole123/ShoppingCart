4998189cc59aabf05c4a645a23681e875212696a
Ankitagole <ankita17@navgurukul.org>
2019-08-21 12:36:26 +0530

    generate Cart class for Map

---
+++ main/java/shoppingCart/Cart.java
@@ -0,0 +1,11 @@
+package shoppingCart;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cart {
+
+    public Map<Product, Integer> getCart(){
+        return new HashMap<>();
+    }
+}
