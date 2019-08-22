86190c3cdaa00db9b6369abc3367af9b85cec49a
Ankitagole <ankita17@navgurukul.org>
2019-08-22 16:22:33 +0530

    create printBill method

--- main/java/user/Customer.java
+++ main/java/user/Customer.java
@@ -1,19 +1,24 @@
 package user;
+import shoppingCart.Bill;
+import shoppingCart.Cart;
+import shoppingCart.Product;
+import shoppingCart.TaxCalculator;
 
-        import shoppingCart.Cart;
-        import shoppingCart.Product;
-        import shoppingCart.TaxCalculator;
-
-        import java.util.Map;
+import java.util.Map;
 
 public class Customer {
     public static void main(String[] args) {
-        Product product = new Product("Chocolate",20,"FOOD",true);
+        Product product1 = new Product("Chocolate",20,"FOOD",true);
+        Product product2 = new Product("ek tha kutta",10,"CD",false);
 
         Cart cart = new Cart();
-        cart.add(product,1);
-
+        cart.add(product1,2);
+        cart.add(product2,1);
         TaxCalculator taxCalculator = new TaxCalculator();
-        
+        Map<Product,Double> tax = taxCalculator.taxCalculation(cart);
+
+        Bill bill = new Bill();
+        bill.generateBill(tax,cart);
+        bill.printBill();
     }
 }
